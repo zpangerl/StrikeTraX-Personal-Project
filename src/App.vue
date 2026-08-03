e86e126166058c8950510c07506d46431e682b9d
Zachary Pangerl <zpangerl@asu.edu>
2026-08-03 11:59:50 -0700

    Added basic navbar across all pages and removed deprecated buttons from home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script setup></script>
 
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/">StrikeTraX</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/">Home
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/scoring">Log Game</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/history">History</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <router-view />
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,25 +3,4 @@
         <h1 class="display-1" style="color: black"><strong>StrikeTraX</strong></h1>
         <p style="color: black">Home Page</p>
     </div>
-    <div class="d-flex justify-content-center">
-        <button @click="enterGame">Enter Game</button>
-        <button @click="enterHistory">History</button>
-    </div>
 </template>
-
-<script>
-    import {useRouter} from 'vue-router'
-
-    export default {
-        setup(){
-            const router = useRouter()
-            function enterGame(){
-                router.push('/scoring')
-            }
-            function enterHistory(){
-                router.push('/history')
-            }
-            return { enterGame, enterHistory }
-        }
-    }
-</script>
