62a5312843593d4d137fa0803bfb280ae115a033
Zachary Pangerl <zpangerl@asu.edu>
2026-08-14 15:25:51 -0700

    Add GET endpoint and update schema to get all required fields

--- backend/main.py
+++ backend/main.py
@@ -2,25 +2,31 @@ import logging
 from database import engine
 from fastapi import FastAPI, HTTPException
 from models import Game
-from schemas import GameStoreRequest
+from schemas import GameStoreRequest, GameListResponse, GameRead
 from scoring import calculate_score
+from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/games")
+def retrieve_games():
+    with Session(engine) as session:
+        try:
+            raw_results = session.execute(select(Game)).scalars().all()
+            converted = [GameRead.model_validate(item) for item in raw_results]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+        except SQLAlchemyError as e:
+            logging.exception("Failed to retrieve games from database")
+            raise HTTPException(status_code=500, detail="Failed to retrieve games, please try again")
+    response = GameListResponse(games=converted)
+    return response
 
 @app.post("/games", status_code=201)
 def store_game(new_game: GameStoreRequest):
     throws = new_game.throws
-    total = new_game.total
+    total = new_game.total_score
     processed_game = calculate_score(throws)
     if (not processed_game["is_valid"] or processed_game["total"] != total):
         # return validation error
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,13 +1,16 @@
+import datetime
 from pydantic import BaseModel, ConfigDict
 
 class GameRead(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     throws: list[int]
     game_id: int
+    total_score: int
+    date: datetime.datetime
 
 class GameStoreRequest(BaseModel):
     throws: list[int]
-    total: int
+    total_score: int
 
 class GameListResponse(BaseModel):
     games: list[GameRead]
